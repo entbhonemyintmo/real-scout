5daa7bf1c86c1d738037197b9d230cbddc78d2ef
Ent Bhone Myint Mo <entbhonemyintmo@gmail.com>
2025-03-05 22:23:05 +0630

    feat(component): search

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -2,21 +2,26 @@ import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native'
 import React from 'react';
 import images from '@/constants/images';
 import icons from '@/constants/icons';
+import Search from '@/components/Search';
 
 const Index = () => {
     return (
         <SafeAreaView className="h-full bg-white">
-            <View className="px-5 mt-5 flex flex-row justify-between items-center">
-                <View className="flex flex-row items-center">
-                    <Image source={images.avatar} className="size-10" />
-                    <View className="ml-2">
-                        <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
-                        <Text className="text-base font-rubik-medium text-black-300">Edward</Text>
+            <View className="px-5">
+                <View className="my-5 flex flex-row justify-between items-center">
+                    <View className="flex flex-row items-center">
+                        <Image source={images.avatar} className="size-10" />
+                        <View className="ml-2">
+                            <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+                            <Text className="text-base font-rubik-medium text-black-300">Edward</Text>
+                        </View>
                     </View>
+                    <TouchableOpacity className="flex flex-row items-center">
+                        <Image source={icons.bell} className="size-6" />
+                    </TouchableOpacity>
                 </View>
-                <TouchableOpacity className="flex flex-row items-center">
-                    <Image source={icons.bell} className="size-6" />
-                </TouchableOpacity>
+
+                <Search />
             </View>
         </SafeAreaView>
     );
--- components/Search.tsx
+++ components/Search.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import React from 'react';
+import icons from '@/constants/icons';
 
 const Search = () => {
-  return (
-    <View>
-      <Text>Search</Text>
-    </View>
-  )
-}
+    return (
+        <View className="flex flex-row items-center justify-between bg-primary-100 rounded-2xl p-3">
+            <View className="flex-1 flex flex-row items-center justify-center z-50">
+                <Image source={icons.search} className="size-5" />
+                <TextInput placeholder="Search" placeholderTextColor="#A0AEC0" className="font-rubik text-black-300 ml-2 flex-1" />
+            </View>
 
-export default Search
+            <TouchableOpacity>
+                <Image source={icons.filter} className="size-5" />
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+export default Search;
