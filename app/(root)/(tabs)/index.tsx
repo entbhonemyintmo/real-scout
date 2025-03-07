5e567d826a6c3e202863ebedd93df63a769076f4
Ent Bhone Myint Mo <entbhonemyintmo@gmail.com>
2025-03-07 23:59:46 +0630

    feat(api): home

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,30 +1,68 @@
-import { View, Text, SafeAreaView, Image, TouchableOpacity, ScrollView, FlatList } from 'react-native';
-import React from 'react';
+import { View, Text, SafeAreaView, Image, TouchableOpacity, ScrollView, FlatList, Button, ActivityIndicator } from 'react-native';
+import React, { useEffect } from 'react';
 import images from '@/constants/images';
 import icons from '@/constants/icons';
 import Search from '@/components/Search';
 import { Card, FeatureCard } from '@/components/Cards';
 import Filter from '@/components/Filter';
+import { useGlobalContext } from '@/lib/global-context-provider';
+import { router, useLocalSearchParams } from 'expo-router';
+import { useFetch } from '@/lib/use-fetch';
+import { getLatestProperties, getProperties } from '@/lib/appwrite';
+import NoResults from '@/components/NoResults';
 
 const Index = () => {
+    const { user } = useGlobalContext();
+
+    const params = useLocalSearchParams<{ query?: string; filter?: string }>();
+
+    const { data: latestProperties, loading: latestPropertiesLoading } = useFetch({
+        fn: getLatestProperties,
+    });
+
+    const {
+        data: properties,
+        refetch,
+        loading,
+    } = useFetch({
+        fn: getProperties,
+        params: {
+            filter: params.filter!,
+            query: params.query!,
+            limit: 6,
+        },
+        skip: true,
+    });
+
+    useEffect(() => {
+        refetch({
+            filter: params.filter!,
+            query: params.query!,
+            limit: 6,
+        });
+    }, [params.filter, params.query]);
+
+    const handleCardPress = (id: string) => router.push(`/properties/${id}`);
+
     return (
         <SafeAreaView className="h-full bg-white">
             <FlatList
-                data={[1, 2, 3]}
-                keyExtractor={(item) => item.toString()}
-                renderItem={({ item }) => <Card />}
+                data={latestProperties}
+                keyExtractor={(item) => item.$id}
+                renderItem={({ item }) => <Card item={item} onPress={() => handleCardPress(item.$id)} />}
                 numColumns={2}
                 columnWrapperClassName="flex gap-5 px-5"
                 contentContainerClassName="pb-32"
                 showsVerticalScrollIndicator={false}
+                ListEmptyComponent={loading ? <ActivityIndicator size="large" className="text-primary-300 mt-5" /> : <NoResults />}
                 ListHeaderComponent={() => (
                     <>
                         <View className="my-5 px-5 flex flex-row justify-between items-center">
                             <View className="flex flex-row items-center">
-                                <Image source={images.avatar} className="size-10" />
+                                <Image source={{ uri: user?.avatar.toString() }} className="size-10 rounded-full" />
                                 <View className="ml-2">
                                     <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
-                                    <Text className="text-base font-rubik-medium text-black-300">Edward</Text>
+                                    <Text className="text-base font-rubik-medium text-black-300">{user?.name}</Text>
                                 </View>
                             </View>
                             <TouchableOpacity className="flex flex-row items-center">
@@ -44,14 +82,21 @@ const Index = () => {
                             </TouchableOpacity>
                         </View>
 
-                        <FlatList
-                            horizontal
-                            data={[1, 2, 3, 4, 5]}
-                            keyExtractor={(item) => item.toString()}
-                            renderItem={({ item }) => <FeatureCard />}
-                            showsHorizontalScrollIndicator={false}
-                            contentContainerClassName="flex gap-5 px-5"
-                        />
+                        {latestPropertiesLoading ? (
+                            <ActivityIndicator size="large" className="text-primary-300" />
+                        ) : !latestProperties || latestProperties.length === 0 ? (
+                            <NoResults />
+                        ) : (
+                            <FlatList
+                                horizontal
+                                numColumns={2}
+                                data={properties}
+                                keyExtractor={(item) => item.$id}
+                                renderItem={({ item }) => <FeatureCard item={item} onPress={() => handleCardPress(item.$id)} />}
+                                showsHorizontalScrollIndicator={false}
+                                contentContainerClassName="flex gap-5 px-5"
+                            />
+                        )}
 
                         {/* Recommendation Section */}
                         <View className="px-5 mt-6 mb-2 flex-row flex justify-between items-center">
--- components/Cards.tsx
+++ components/Cards.tsx
@@ -2,30 +2,36 @@ import { View, Text, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
 import images from '@/constants/images';
 import icons from '@/constants/icons';
+import { Models } from 'react-native-appwrite';
 
-const FeatureCard = () => {
+interface Props {
+    item: Models.Document;
+    onPress?: () => void;
+}
+
+const FeatureCard = ({ item, onPress }: Props) => {
     return (
-        <TouchableOpacity className="flex flex-col items-start w-60 h-80 relative">
+        <TouchableOpacity onPress={onPress} className="flex flex-col items-start w-60 h-80 relative">
             {/* Image Background */}
-            <Image source={images.japan} className="size-full rounded-2xl" />
+            <Image source={{ uri: item.image }} className="size-full rounded-2xl" />
             <Image source={images.cardGradient} className="size-full absolute rounded-2xl" />
 
             {/* Rating */}
             <View className="flex flex-row items-center absolute top-5 right-5 bg-white/80 py-1 px-2 rounded-full">
                 <Image source={icons.star} className="size-4 mr-1" />
-                <Text className="text-sm font-rubik-bold text-primary-300">4.5</Text>
+                <Text className="text-sm font-rubik-bold text-primary-300">{item.rating}</Text>
             </View>
 
             {/* Info */}
             <View className="flex flex-col items-start absolute bottom-5 left-5 w-4/6">
                 <Text className="text-lg font-rubik-extrabold text-white" numberOfLines={1}>
-                    Aung Myay Tharsi Villa
+                    {item.name}
                 </Text>
                 <Text className="text-sm font-rubik text-white w-4/6" numberOfLines={1}>
-                    Kamayut, Yangon
+                    {item.address}
                 </Text>
                 <Text className="text-lg font-rubik-extrabold text-white w-full" numberOfLines={1}>
-                    $130500
+                    ${item.price}
                 </Text>
             </View>
 
@@ -37,25 +43,25 @@ const FeatureCard = () => {
     );
 };
 
-const Card = () => {
+const Card = ({ item, onPress }: Props) => {
     return (
-        <TouchableOpacity className="mb-5 flex p-3 rounded-2xl bg-white shadow-md shadow-black-100/30 relative">
+        <TouchableOpacity onPress={onPress} className="mb-5 flex p-3 rounded-2xl bg-white shadow-md shadow-black-100/30 relative">
             <View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
                 <Image source={icons.star} className="size-2.5" />
-                <Text className="text-xs font-rubik-bold text-primary-300 ml-0.5">4.5</Text>
+                <Text className="text-xs font-rubik-bold text-primary-300 ml-0.5">{item.rating}</Text>
             </View>
 
-            <Image source={images.japan} className="w-full h-40 rounded-lg" />
+            <Image source={{ uri: item.image }} className="w-full h-40 rounded-lg" />
 
             <View className="flex flex-col items-start mt-3">
                 <Text className="text-sm font-rubik-extrabold text-black-300" numberOfLines={1}>
-                    Aung Myay Tharsi Villa
+                    {item.name}
                 </Text>
                 <Text className="text-xs font-rubik text-black-100 w-4/6" numberOfLines={1}>
-                    Kamayut, Yangon
+                    {item.address}
                 </Text>
                 <Text className="text-md font-rubik-extrabold text-primary-300 w-full" numberOfLines={1}>
-                    $130500
+                    ${item.price}{' '}
                 </Text>
             </View>
 
